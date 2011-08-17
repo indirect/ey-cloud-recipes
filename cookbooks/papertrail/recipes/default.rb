54ef55e641346a051f94f73578bbe586e1117e4e
Andre Arko <andre@arko.net>
2011-08-16 18:55:48 -0700

    papertrail, now with gentoo init script and syslog uploads

--- cookbooks/papertrail/recipes/default.rb
+++ cookbooks/papertrail/recipes/default.rb
@@ -4,52 +4,58 @@
 #
 # This recipe configures instances to send logs to Papertrail (papertrailapp.com)
 # and installs the remote_syslog daemon to tail any log to the service.
+# A second remote_syslog daemon to monitor /var/log/syslog is included because
+# the syslog version on EY instances is too old to support logging to a given
+# port. That means syslog to Papertrail won't work, so we work around it.
 
-# Turns out that syslog on EY images is too old to log to a port :(
-# execute "configure syslog" do
-#   command %{
-#     echo '*.*          @logs.papertrailapp.com:50505' >> /etc/syslog.conf
-#     killall -HUP syslogd
-#   }
-#   not_if { system("grep papertrailapp /etc/syslog.conf &> /dev/null") }
-# end
+PORT_NUMBER = 'XXXXX'
 
-# remote_syslog daemon to pass logs to papertrail
-template "/etc/log_files.yml" do
-  source "log_files.yml.erb"
-  owner "root"
-  group "root"
-  mode "0644"
-  # PUT YOUR PORT NUMBER HERE
-  variables(:port_number => '')
-end
+app_name = node[:applications].keys.first
+env = node[:environment][:framework_env]
+PAPERTRAIL_CONFIG = {
+  :port_number => PORT_NUMBER,
+  :hostname => "#{app_name}_#{env[0..0]}_#{`hostname`.chomp}"
+}
 
-execute "install new enough of eventmachine" do
-  # install manually because we get 0.12.6 and we need 0.12.10
-  command %{gem install eventmachine}
-  creates "/usr/lib/ruby/gems/1.8/cache/eventmachine-0.12.10.gem"
-end
-
-execute "install remote_syslog" do
-  command %{gem install remote_syslog}
+execute "install remote_syslog gem from rubygems" do
+  command %{gem install remote_syslog -v "~>1.2"}
   creates "/usr/bin/remote_syslog"
 end
 
-# Automate running remote_syslog
-remote_file "install remote_syslog init.d script" do
-  action :create_if_missing
-  path "/etc/init.d/remote_syslog"
-  source "https://raw.github.com/papertrail/remote_syslog/master/examples/remote_syslog.init.d"
-  owner "root"
-  group "root"
-  mode "0755"
-end
+# remote_syslog daemons, one to handle regular logs,
+%w(default syslog).each do |name|
+  # remote_syslog config file
+  template "/etc/log_files_#{name}.yml" do
+    source "log_files_#{name}.yml.erb"
+    mode "0644"
+    variables(PAPERTRAIL_CONFIG)
+  end
 
-execute "start remote_syslog at boot" do
-  command %{rc-update add remote_syslog default}
-  not_if { system("rc-update -s | grep remote_syslog &> /dev/null") }
-end
+  # init.d config file
+  template "/etc/conf.d/remote_syslog_#{name}" do
+    source "remote_syslog.confd.erb"
+    mode "0644"
+    # Giving the default remote_syslog a custom pid works around a bug
+    # in Daemons that prevents it from starting after other copies.
+    variables(
+      :config => "/etc/log_files_#{name}.yml",
+      :name => "remote_syslog_#{name}")
+  end
 
-execute "start or restart remote_syslog" do
-  command %{/etc/init.d/remote_syslog restart}
+  # init.d script
+  template "/etc/init.d/remote_syslog_#{name}" do
+    source "remote_syslog.initd.erb"
+    mode "0755"
+  end
+
+  # start at boot
+  execute "start remote_syslog_#{name} at boot" do
+    command %{rc-update add remote_syslog_#{name} default}
+    creates "/etc/runlevels/default/remote_syslog_#{name}"
+  end
+
+  # start right now
+  execute "start or restart remote_syslog" do
+    command %{/etc/init.d/remote_syslog_#{name} restart}
+  end
 end
